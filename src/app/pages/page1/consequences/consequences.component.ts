a208080a9215eb735fb875e3e3a1a5131366cd56
Florian Salengros <florian.salengros@gmail.com>
2025-06-18 15:08:53 +0200

    optimisation images

---
+++ src/app/pages/page1/consequences/consequences.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consequences',
+  imports: [],
+  templateUrl: './consequences.component.html',
+  styleUrl: './consequences.component.scss'
+})
+export class ConsequencesComponent {
+
+}
