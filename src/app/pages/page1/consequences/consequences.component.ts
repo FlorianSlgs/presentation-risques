78e07269dffb481c30b11a337e3c982ffe378e58
Florian Salengros <florian.salengros@gmail.com>
2025-06-19 14:08:34 +0200

    map

--- src/app/pages/page1/consequences/consequences.component.ts
+++ src/app/pages/page1/consequences/consequences.component.ts
@@ -68,9 +68,9 @@ export class ConsequencesComponent implements OnInit, AfterViewInit {
     villes.forEach(ville => {
       const popupContent = `
         <b>${ville.nom}</b><br>
-        ${ville.date ? `<span class="font-semibold">Date :</span> ${ville.date}<br>` : ''}
-        ${ville.degats ? `<span class="font-semibold">Dégâts :</span> ${ville.degats}<br>` : ''}
-        ${ville.couts ? `<span class="font-semibold">Coûts :</span> ${ville.couts}` : ''}
+        ${ville.date ? `${ville.date}<br>` : ''}
+        ${ville.degats ? `${ville.degats}<br>` : ''}
+        ${ville.couts ? `${ville.couts}` : ''}
       `;
       const marker: L.Marker = L.marker(ville.coords, { icon: customIcon })
         .addTo(this.map)
@@ -89,78 +89,3 @@ export class ConsequencesComponent implements OnInit, AfterViewInit {
     }
   }
 }
-
-/* import { Component, OnInit, AfterViewInit } from '@angular/core';
-import * as L from 'leaflet';
-
-@Component({
-  selector: 'app-consequences',
-  imports: [],
-  templateUrl: './consequences.component.html',
-  styleUrl: './consequences.component.scss'
-})
-export class ConsequencesComponent implements OnInit, AfterViewInit {
-  private map!: L.Map;
-  private centroid: L.LatLngExpression = [43.5797, 3.3672]; // Paris coordinates
-
-  constructor() { }
-
-  ngOnInit(): void {
-    // Initialisation du composant
-  }
-
-  ngAfterViewInit(): void {
-    this.initMap();
-    this.addMarker();
-  }
-
-  private initMap(): void {
-    // Initialiser la carte
-    this.map = L.map('map', {
-      center: this.centroid,
-      zoom: 8
-    });
-
-    // Ajouter les tuiles OpenStreetMap
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-
-    tiles.addTo(this.map);
-  }
-
-  private addMarker(): void {
-    // Créer une icône personnalisée
-    const customIcon = L.icon({
-      iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      shadowSize: [41, 41]
-    });
-
-    // Ajouter un marqueur
-    const marker = L.marker(this.centroid, { icon: customIcon }).addTo(this.map);
-    
-    // Ajouter un popup au marqueur
-    marker.bindPopup('<b>Paris</b><br>Capitale de la France').openPopup();
-
-    // Ajouter un cercle
-    const circle = L.circle([48.8566, 2.3522], {
-      color: 'red',
-      fillColor: '#f03',
-      fillOpacity: 0.3,
-      radius: 1000
-    }).addTo(this.map);
-
-    circle.bindPopup('Zone de 1km autour de Paris');
-
-    // Événement de clic sur la carte
-    this.map.on('click', (e: L.LeafletMouseEvent) => {
-      console.log('Cliqué aux coordonnées:', e.latlng);
-    });
-  }
-} */
