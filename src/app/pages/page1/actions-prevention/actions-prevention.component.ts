c295330e38806ba884e1890fa680cbbdb6a0cdd6
Florian Salengros <florian.salengros@gmail.com>
2025-08-01 15:55:58 +0200

    papi et ppri adapté

--- src/app/pages/page1/actions-prevention/actions-prevention.component.ts
+++ src/app/pages/page1/actions-prevention/actions-prevention.component.ts
@@ -14,14 +14,13 @@ export class ActionsPreventionComponent {
 selectedTab = 0;
   tabs = [
     {
+      label: 'Logement',
+    },
+    {
       label: 'PAPI',
-      content: 'Je contribue également à ma protection dans mon logement. Si ma commune relève d’un programme d’actions de prévention des inondations :',
-      content1 : 'Je contribue également à ma protection dans mon logement. Si ma commune est couverte par un programme d’actions de prévention des inondations, je me rapproche de l’EPTB compétent et des diagnostiqueurs qui me conseilleront sur les mesures adaptées à mon logement. ',
     },
     {
       label: 'PPRi',
-      content: 'En l’absence de PAPI, si ma commune est couverte par un plan de prévention des risques naturels d’inondation :',
-      content1: 'En l’absence de PAPI, si ma commune est couverte par un plan de prévention des risques naturels d’inondation, je peux avoir une obligation règlementaire de réaliser des travaux de mise en sécurité en cas d’inondation.',
     }
   ];
 }
